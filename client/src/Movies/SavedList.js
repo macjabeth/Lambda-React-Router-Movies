4f9d5b5b522143ca3397cc261865f1da5a0ffe08
macjabeth <macbethjonathan@gmail.com>
2019-08-24 17:26:59 -0400

    add movie saving functionality

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <NavLink to={`/movies/${movie.id}`}>
+        <span className="saved-movie">{movie.title}</span>
+      </NavLink>
     ))}
-    <div className="home-button">Home</div>
+    <div className="home-button">
+      <NavLink exact to="/">Home</NavLink>
+    </div>
   </div>
 );
 
